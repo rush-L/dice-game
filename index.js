6a95dd279e6a0759aafc13e4c9be2906b920f4cf
Russ <reambillo.russel@gmail.com>
2024-04-01 00:38:21 +0800

    add some js codes

---
+++ index.js
@@ -0,0 +1,9 @@
+const img1 = document.getElementsByClassName("img1")
+
+var randomNum1 = Math.floor(Math.random()*6)+1;
+
+console.log(document.getElementsByTagName("h1"));
+
+function random(){
+    
+}
